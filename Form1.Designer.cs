2e749db0598267aa0af4a90e0ab7f832b4ead7e5
Mervan-Torlak <mervan.torlak@ogr.sakarya.edu.tr>
2023-06-12 16:47:02 +0300

    Update

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Oyun
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.oyuncu = new System.Windows.Forms.PictureBox();
+            this.solTimer2 = new System.Windows.Forms.Timer(this.components);
+            this.sagTimer3 = new System.Windows.Forms.Timer(this.components);
+            this.asagiTimer4 = new System.Windows.Forms.Timer(this.components);
+            this.yukariTimer5 = new System.Windows.Forms.Timer(this.components);
+            this.mermiTimer2 = new System.Windows.Forms.Timer(this.components);
+            this.dusmanTimer2 = new System.Windows.Forms.Timer(this.components);
+            this.dusmanMermiTimer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.cikisButton1 = new System.Windows.Forms.Button();
+            this.baslatButton2 = new System.Windows.Forms.Button();
+            this.puanLabel2 = new System.Windows.Forms.Label();
+            this.levelLabel2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.oyuncu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // oyuncu
+            // 
+            this.oyuncu.BackColor = System.Drawing.Color.Transparent;
+            this.oyuncu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.oyuncu.Image = ((System.Drawing.Image)(resources.GetObject("oyuncu.Image")));
+            this.oyuncu.Location = new System.Drawing.Point(272, 381);
+            this.oyuncu.Name = "oyuncu";
+            this.oyuncu.Size = new System.Drawing.Size(42, 60);
+            this.oyuncu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.oyuncu.TabIndex = 0;
+            this.oyuncu.TabStop = false;
+            // 
+            // solTimer2
+            // 
+            this.solTimer2.Interval = 5;
+            this.solTimer2.Tick += new System.EventHandler(this.solTimer2_Tick);
+            // 
+            // sagTimer3
+            // 
+            this.sagTimer3.Interval = 5;
+            this.sagTimer3.Tick += new System.EventHandler(this.sagTimer3_Tick);
+            // 
+            // asagiTimer4
+            // 
+            this.asagiTimer4.Interval = 5;
+            this.asagiTimer4.Tick += new System.EventHandler(this.asagiTimer4_Tick);
+            // 
+            // yukariTimer5
+            // 
+            this.yukariTimer5.Interval = 5;
+            this.yukariTimer5.Tick += new System.EventHandler(this.yukariTimer5_Tick);
+            // 
+            // mermiTimer2
+            // 
+            this.mermiTimer2.Enabled = true;
+            this.mermiTimer2.Interval = 20;
+            this.mermiTimer2.Tick += new System.EventHandler(this.mermiTimer2_Tick);
+            // 
+            // dusmanTimer2
+            // 
+            this.dusmanTimer2.Enabled = true;
+            this.dusmanTimer2.Tick += new System.EventHandler(this.dusmanTimer2_Tick);
+            // 
+            // dusmanMermiTimer2
+            // 
+            this.dusmanMermiTimer2.Enabled = true;
+            this.dusmanMermiTimer2.Interval = 20;
+            this.dusmanMermiTimer2.Tick += new System.EventHandler(this.dusmanMermiTimer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("MV Boli", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(558, 87);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Label 1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Visible = false;
+            // 
+            // cikisButton1
+            // 
+            this.cikisButton1.BackColor = System.Drawing.Color.Red;
+            this.cikisButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cikisButton1.ForeColor = System.Drawing.Color.Black;
+            this.cikisButton1.Location = new System.Drawing.Point(227, 309);
+            this.cikisButton1.Name = "cikisButton1";
+            this.cikisButton1.Size = new System.Drawing.Size(144, 41);
+            this.cikisButton1.TabIndex = 2;
+            this.cikisButton1.Text = "ÇIKIŞ";
+            this.cikisButton1.UseVisualStyleBackColor = false;
+            this.cikisButton1.Visible = false;
+            this.cikisButton1.Click += new System.EventHandler(this.cikisButton1_Click);
+            // 
+            // baslatButton2
+            // 
+            this.baslatButton2.BackColor = System.Drawing.Color.Red;
+            this.baslatButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.baslatButton2.ForeColor = System.Drawing.Color.Black;
+            this.baslatButton2.Location = new System.Drawing.Point(227, 231);
+            this.baslatButton2.Name = "baslatButton2";
+            this.baslatButton2.Size = new System.Drawing.Size(144, 41);
+            this.baslatButton2.TabIndex = 3;
+            this.baslatButton2.Text = "BAŞLAT";
+            this.baslatButton2.UseVisualStyleBackColor = false;
+            this.baslatButton2.Visible = false;
+            this.baslatButton2.Click += new System.EventHandler(this.baslatButton2_Click);
+            // 
+            // puanLabel2
+            // 
+            this.puanLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.puanLabel2.ForeColor = System.Drawing.Color.DarkOrange;
+            this.puanLabel2.Location = new System.Drawing.Point(3, 9);
+            this.puanLabel2.Name = "puanLabel2";
+            this.puanLabel2.Size = new System.Drawing.Size(163, 26);
+            this.puanLabel2.TabIndex = 4;
+            this.puanLabel2.Text = "Puan: 0";
+            // 
+            // levelLabel2
+            // 
+            this.levelLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.levelLabel2.ForeColor = System.Drawing.Color.DarkOrange;
+            this.levelLabel2.Location = new System.Drawing.Point(475, 9);
+            this.levelLabel2.Name = "levelLabel2";
+            this.levelLabel2.Size = new System.Drawing.Size(103, 26);
+            this.levelLabel2.TabIndex = 5;
+            this.levelLabel2.Text = "Level:1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(582, 453);
+            this.Controls.Add(this.baslatButton2);
+            this.Controls.Add(this.cikisButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.oyuncu);
+            this.Controls.Add(this.puanLabel2);
+            this.Controls.Add(this.levelLabel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.oyuncu)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox oyuncu;
+        private System.Windows.Forms.Timer solTimer2;
+        private System.Windows.Forms.Timer sagTimer3;
+        private System.Windows.Forms.Timer asagiTimer4;
+        private System.Windows.Forms.Timer yukariTimer5;
+        private System.Windows.Forms.Timer mermiTimer2;
+        private System.Windows.Forms.Timer dusmanTimer2;
+        private System.Windows.Forms.Timer dusmanMermiTimer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cikisButton1;
+        private System.Windows.Forms.Button baslatButton2;
+        private System.Windows.Forms.Label puanLabel2;
+        private System.Windows.Forms.Label levelLabel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace Oyun
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer oyunSesi;
+        WindowsMediaPlayer mermiSesi;
+        WindowsMediaPlayer patlamaSesi;
+        PictureBox[] yildizlar;
+        PictureBox[] mermi;
+        PictureBox[] dusmanlar;
+        PictureBox[] dusmanMermisi;
+        int arkaPlanHizi;
+        int oyuncuHizi;
+        int mermiHizi;
+        int dusmanHizi;
+        int dusmanMermiHizi;
+        int puan;
+        int level;
+        int zorluk;
+        bool durdur;
+        bool oyunSonaErdi;
+        Random rnd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            durdur = false;
+            oyunSonaErdi = false;
+            puan = 0;
+            level = 1;
+            zorluk = 9;
+            arkaPlanHizi = 4;
+            dusmanHizi = 4;
+            oyuncuHizi = 4;
+            mermiHizi = 20;
+            dusmanMermiHizi = 4;
+            yildizlar = new PictureBox[13];
+            mermi = new PictureBox[3];
+            dusmanMermisi = new PictureBox[10];
+            Image mermiler = Image.FromFile(@"asserts\\mermi2.png");
+            rnd = new Random();
+
+            Image dusman1 = Image.FromFile(@"asserts\\düşman1.png");
+            Image dusman2 = Image.FromFile(@"asserts\\düşman 2.png");
+            Image dusman3 = Image.FromFile(@"asserts\\düşman3.png");
+            Image boss1 = Image.FromFile(@"asserts\\boss1.png");
+            Image boss2 = Image.FromFile(@"asserts\\boss2.png");
+
+            dusmanlar = new PictureBox[10];
+
+            for (int i = 0; i < dusmanlar.Length; i++)
+            {
+                dusmanlar[i] = new PictureBox();
+                dusmanlar[i].Size = new Size(40, 40);
+                dusmanlar[i].SizeMode = PictureBoxSizeMode.Zoom;
+                dusmanlar[i].BorderStyle = BorderStyle.None;
+                dusmanlar[i].Visible = false;
+                this.Controls.Add(dusmanlar[i]);
+                dusmanlar[i].Location = new Point((i + 1) * 50, -50);
+            }
+
+            dusmanlar[0].Image = boss1;
+            dusmanlar[1].Image = dusman2;
+            dusmanlar[2].Image = dusman3;
+            dusmanlar[3].Image = boss2;
+            dusmanlar[4].Image = dusman1;
+            dusmanlar[5].Image = dusman3;
+            dusmanlar[6].Image = dusman2;
+            dusmanlar[7].Image = boss2;
+            dusmanlar[8].Image = dusman2;
+            dusmanlar[9].Image = dusman3;
+
+            for (int i = 0; i < mermi.Length; i++)
+            {
+                mermi[i] = new PictureBox();
+                mermi[i].Size = new Size(12, 12);
+                mermi[i].Image = mermiler;
+                mermi[i].SizeMode = PictureBoxSizeMode.Zoom;
+                mermi[i].BorderStyle = BorderStyle.None;
+                this.Controls.Add(mermi[i]);
+            }
+
+            for (int i = 0; i < dusmanMermisi.Length; i++)
+            {
+                dusmanMermisi[i] = new PictureBox();
+                dusmanMermisi[i].Size = new Size(2, 20);
+                dusmanMermisi[i].Visible = false;
+                dusmanMermisi[i].BackColor = Color.Red;
+                int x = rnd.Next(0, 10);
+                dusmanMermisi[i].Location = new Point(dusmanlar[x].Location.X, dusmanlar[x].Location.Y-20 );
+                this.Controls.Add(dusmanMermisi[i]);
+
+            }
+
+            oyunSesi = new WindowsMediaPlayer();
+            mermiSesi = new WindowsMediaPlayer();
+            patlamaSesi = new WindowsMediaPlayer();
+
+            oyunSesi.URL = "songs\\GameSong.mp3";
+            mermiSesi.URL = "songs\\shoot.mp3";
+            patlamaSesi.URL = "songs\\boom.mp3";
+
+            oyunSesi.settings.setMode("loop", true);
+            oyunSesi.settings.volume = 5;
+            mermiSesi.settings.volume = 1;
+            patlamaSesi.settings.volume = 6;
+            oyunSesi.controls.play();
+
+
+            for (int i = 0; i < yildizlar.Length; i++)
+            {
+                yildizlar[i] = new PictureBox();
+                yildizlar[i].BorderStyle = BorderStyle.None;
+                yildizlar[i].Location = new Point(rnd.Next(10, 500), rnd.Next(-10, 400));
+
+                if (i % 2 == i) 
+                {
+                    yildizlar[i].Size = new Size(3, 2);
+                    yildizlar[i].BackColor = Color.White;
+                }
+                else
+                {
+                    yildizlar[i].Size = new Size(4, 4);
+                    yildizlar[i].BackColor = Color.DarkGray;
+                }
+                this.Controls.Add(yildizlar[i]);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < yildizlar.Length; i++)
+            {
+                yildizlar[i].Top += arkaPlanHizi;
+                if (yildizlar[i].Top >= this.Height)
+                {
+                    yildizlar[i].Top = -yildizlar[i].Height;
+                }
+            }
+
+            for (int i= yildizlar.Length / 2; i < yildizlar.Length; i++)
+            {
+                yildizlar[i].Top += arkaPlanHizi - 2;
+                if (yildizlar[i].Top >= this.Height)
+                {
+                    yildizlar[i].Top = -yildizlar[i].Height;
+                }
+            }
+        }
+
+        private void solTimer2_Tick(object sender, EventArgs e)
+        {
+            if (oyuncu.Left > 10)
+            {
+                oyuncu.Left -= oyuncuHizi;
+            }
+        }
+
+        private void sagTimer3_Tick(object sender, EventArgs e)
+        {
+            if (oyuncu.Right < 435)
+            {
+                oyuncu.Left += oyuncuHizi;
+            }
+        }
+
+        private void asagiTimer4_Tick(object sender, EventArgs e)
+        {
+            if (oyuncu.Top < 320)
+            {
+                oyuncu.Top += oyuncuHizi;
+            }
+        }
+
+        private void yukariTimer5_Tick(object sender, EventArgs e)
+        {
+            if (oyuncu.Top > 10) 
+            {
+                oyuncu.Top -= oyuncuHizi;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(!durdur)
+            {
+                if (e.KeyCode == Keys.Right)
+                {
+                    sagTimer3.Start();
+                }
+                if (e.KeyCode == Keys.Left)
+                {
+                    solTimer2.Start();
+                }
+                if (e.KeyCode == Keys.Down)
+                {
+                    asagiTimer4.Start();
+                }
+                if (e.KeyCode == Keys.Up)
+                {
+                    yukariTimer5.Start();
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            sagTimer3.Stop();
+            solTimer2.Stop();
+            asagiTimer4.Stop();
+            yukariTimer5.Stop();
+
+            if(e.KeyCode==Keys.Space)
+            {
+                if(!oyunSonaErdi)
+                {
+                    if(durdur)
+                    {
+                        saatleriBaslat();
+                        label1.Visible = false;
+                        oyunSesi.controls.play(); ;
+                        durdur = false;
+                    }
+                    else
+                    {
+                        
+                        label1.Text = "DURDURULDU";
+                        label1.Visible = true;
+                        oyunSesi.controls.pause();
+                        saatleriDurdur();
+                        durdur = true;
+                    }
+                }
+            }
+            
+        }
+
+        private void mermiTimer2_Tick(object sender, EventArgs e)
+        {
+            mermiSesi.controls.play();
+            for (int i = 0; i < mermi.Length; i++)
+            {
+                if (mermi[i].Top > 0)
+                {
+                    mermi[i].Visible = true;
+                    mermi[i].Top -= mermiHizi;
+
+                    patlama();
+
+                }
+                else
+                {
+                    mermi[i].Visible = false;
+                    mermi[i].Location = new Point(oyuncu.Location.X + 15, oyuncu.Location.Y - i * 30);
+                }
+            }
+        
+        }
+
+        private void dusmanTimer2_Tick(object sender, EventArgs e)
+        {
+            dusmanHareketi(dusmanlar, dusmanHizi);
+        }
+
+        private void dusmanHareketi(PictureBox[] array, int hız)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i].Visible = true;
+                array[i].Top += hız;
+
+                if(array[i].Top>this.Height)
+                {
+                    array[i].Location = new Point((i + 1) * 50, -200);
+                }
+            }
+        }
+
+        private void patlama()
+        {
+            for (int i = 0; i < dusmanlar.Length; i++)
+            {
+                if (mermi[0].Bounds.IntersectsWith(dusmanlar[i].Bounds) || mermi[1].Bounds.IntersectsWith(dusmanlar[i].Bounds) || mermi[2].Bounds.IntersectsWith(dusmanlar[i].Bounds))
+                {
+                    patlamaSesi.controls.play();
+                    puan += 1;
+                    puanLabel2.Text = (puan > 10) ? "Puan:   " + puan.ToString() : puan.ToString();
+
+
+
+                    if (puan%30==0)
+                    {
+                        level += 1;
+                        levelLabel2.Text = (level < 10) ? "Level:0" + level.ToString() : level.ToString();
+
+                        if (dusmanHizi <= 10 && dusmanMermiHizi <= 10 && zorluk >= 0)
+                        {
+                            zorluk --;
+                            dusmanHizi++;
+                            dusmanMermiHizi++;
+                        }
+                        if(level==10)
+                        {
+                            oyunBitti("TEBRİKLER");
+                        }
+                    }
+
+
+                    if (oyuncu.Bounds.IntersectsWith(dusmanlar[i].Bounds))
+                    {
+                        patlamaSesi.settings.volume = 30;
+                        patlamaSesi.controls.play();
+                        oyuncu.Visible = false;
+                        oyunBitti("OYUN BİTTİ");
+                    }
+                    dusmanlar[i].Location = new Point((i + 1) * 50, -100);
+                }
+            }
+        }
+
+        private void oyunBitti(String str)
+        {
+            label1.Text = str;
+            label1.Visible = true;
+            baslatButton2.Visible = true;
+            cikisButton1.Visible = true;
+            oyunSesi.controls.stop();
+            saatleriDurdur();
+        }
+
+        private void saatleriDurdur()
+        {
+            timer1.Stop();
+            dusmanTimer2.Stop();
+            mermiTimer2.Stop();
+            dusmanMermiTimer2.Stop();
+        }
+        
+        private void saatleriBaslat()
+        {
+            timer1.Start();
+            dusmanTimer2.Start();
+            mermiTimer2.Start();
+            dusmanMermiTimer2.Start();
+        }
+
+        private void dusmanMermiTimer2_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < (dusmanMermisi.Length-zorluk); i++)
+            {
+                if (dusmanMermisi[i].Top < this.Height)
+                {
+                    dusmanMermisi[i].Visible = true;
+                    dusmanMermisi[i].Top += dusmanMermiHizi;
+
+                    dusmanMermiPatlaması();
+                }
+                else
+                {
+                    dusmanMermisi[i].Visible = false;
+                    int x = rnd.Next(0, 10);
+                    dusmanMermisi[i].Location = new Point(dusmanlar[x].Location.X + 20, dusmanlar[x].Location.Y + 30);              
+                }
+            }
+            
+        }
+
+        private void dusmanMermiPatlaması()
+        {
+            for (int i = 0; i < dusmanMermisi.Length; i++)
+            {
+                if(dusmanMermisi[i].Bounds.IntersectsWith(oyuncu.Bounds))
+                {
+                    dusmanMermisi[i].Visible = false;
+                    patlamaSesi.settings.volume = 30;
+                    patlamaSesi.controls.play();
+                    oyuncu.Visible = false;
+                    oyunBitti("Oyun Bitti");
+                }
+            }
+        }
+
+        private void cikisButton1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void baslatButton2_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            InitializeComponent();
+            Form1_Load(e,e);
+        }
+    }
+}
